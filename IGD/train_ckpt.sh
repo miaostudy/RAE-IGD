cb414d87ca35a6fa2436233c20b7856d896dc8d6
miaostudy <2775921733@qq.com>
2025-12-16 12:35:12 +0800

    fix train_ckpt.sh

--- IGD/train_ckpt.sh
+++ IGD/train_ckpt.sh
@@ -2,6 +2,6 @@
 model=convnet6
 spec=1k
 
-python IGD/train_ckpts.py -d imagenet --imagenet_dir /data2/wlf/datasets/imagenet/train/ /data2/wlf/datasets/imagenet/val/ \
+python IGD/train_ckpts.py -d imagenet --imagenet_dir /data2/wlf/datasets/imagenet/train/ /data2/wlf/datasets/imagenet \
     -n ${model} --nclass 1000 --norm_type instance --tag test --slct_type random --spec ${spec} --mixup vanilla --repeat 1 \
     --ckpt-dir ./ckpts/${spec}/${model}/ --epochs 50 --depth 6 --ipc -1
